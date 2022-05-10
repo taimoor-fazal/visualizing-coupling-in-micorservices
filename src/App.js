2b9fca50bc0b8a085f817230ae2e564e39c41205
Taimoor Fazal <taimoorf@uio.no>
2022-05-10 18:24:46 +0200

    fix: implementation of line-charts and coupling evolution

--- src/App.js
+++ src/App.js
@@ -1,149 +1,38 @@
 
 import './App.css';
 import jsonGraph from './dependency graphs/1.6.2.json';
-import ForceGraph3D from 'react-force-graph-2d';
+import systemEvolution from './release data/system-evolution.json'
+import ForceGraph3D from 'react-force-graph-3d';
 import { useState } from 'react';
+import {
+  LineChart,
+  ResponsiveContainer,
+  Legend, Tooltip,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid
+} from 'recharts';
+
+
 
 const dependencyGraph = jsonGraph;
 let microservicesData = transformedData();
 const rootMicroserviceId = microservicesData.nodes.find(x => x.name === "Root").id;
 
-function dummyData() {
-  let json = {
-    "nodes": [
-      {
-        "id": "id1",
-        "name": "name1",
-        "val": 1
-      },
-      {
-        "id": "id2",
-        "name": "name2",
-        "val": 10
-      },
-      {
-        "id": "id3",
-        "name": "name3",
-        "val": 50
-      },
-      {
-        "id": "id4",
-        "name": "name4",
-        "val": 30
-      },
-      {
-        "id": "id5",
-        "name": "name5",
-        "val": 13
-      },
-
-    ],
-    "links": [
-      {
-        "source": "id1",
-        "target": "id2"
-      },
-      {
-        "source": "id2",
-        "target": "id3"
-      }, {
-        "source": "id2",
-        "target": "id4"
-      }, {
-        "source": "id3",
-        "target": "id5"
-      },
-      {
-        "source": "id3",
-        "target": "id1"
-      },
-      {
-        "source": "id1",
-        "target": "id5"
-      }
 
 
-
-    ]
-  };
-  return json;
-
-
-}
-function transformedData() {
-
-  let nodes = [];
-  let links = [];
-  let nodeObject;
-  let edgeObject;
-
-
-
-
-  for (let edge of dependencyGraph.elements.edges) {
-    edgeObject = {};
-    edgeObject.source = edge.data.source;
-    edgeObject.target = edge.data.target;
-
-    links.push(edgeObject);
-  }
-  for (let node of dependencyGraph.elements.nodes) {
-    nodeObject = {};
-    //the API gateway service has no name. So we need to avoid that
-    if (node.data.service)
-      nodeObject.name = node.data.service;
-
-    else
-
-      nodeObject.name = "Root"
-
-    nodeObject.value = 1;
-    nodeObject.id = node.data.id;
-    nodeObject.ads = links.filter(x => x.source === node.data.id).length;
-    //not inlcuding any links for the root microservice
-    nodeObject.ais = links.filter(x => x.target === node.data.id && node.data.service).length;
-    nodes.push(nodeObject);
-  }
-  console.log(nodes);
-  return { "nodes": nodes, "links": links };
-
+function getRootId(json) {
+  if (json)
+    return json.nodes.find(x => x.data.isRoot).data.id;
+  else
+    return microservicesData.nodes.find(x => x.data.isRoot).data.id;
 }
 
-// function genRandomTree(N = 300, reverse = false) {
-//   return {
-//     nodes: [...Array(N).keys()].map(i => ({ id: i })),
-//     links: [...Array(N).keys()]
-//       .filter(id => id)
-//       .map(id => ({
-//         [reverse ? 'target' : 'source']: id,
-//         [reverse ? 'source' : 'target']: Math.round(Math.random() * (id - 1))
-//       }))
-//   };
-// }
 function App() {
-  console.log(dependencyGraph);
-  const [showData, setShowData] = useState(false);
-  // let myData = {
-  //   "nodes": [
-  //     {
-  //       "id": "id1",
-  //       "name": "name1",
-  //       "val": 100
-  //     },
-  //     {
-  //       "id": "id2",
-  //       "name": "name2",
-  //       "val": 10
-  //     }
 
-  //   ],
-  //   "links": [
-  //     {
-  //       "source": "id1",
-  //       "target": "id2"
-  //     }
-  //   ]
-  // }
+  const [showData, setShowData] = useState(false);
+
 
   return (
 
@@ -151,13 +40,16 @@ function App() {
 
     <>
       <div className='header'>
-        <h2>Measuring Coupling in Microservices</h2>
+        <h3>Measuring Coupling in Microservices</h3>
       </div>
       <div className='container'>
 
         <Output setShowData={setShowData} />
         <h1>Microservice Level Details</h1>
         <SystemInformation />
+        <h1>Coupling Evolution</h1>
+        <CouplingEvolution />
+
       </div>
 
 
@@ -181,8 +73,7 @@ function Graph() {
 
   microservicesData = [];
   microservicesData = transformedData();
-  console.log("Graph");
-  console.log(microservicesData);
+
   const [zoom, setZoom] = useState(5);
   return (
     <div>
@@ -200,7 +91,7 @@ function Graph() {
           backgroundColor={"#424760"}
           linkDirectionalArrowLength={3.5}
           linkDirectionalArrowRelPos={1}
-          linkCurvature={0.25}
+          linkCurvature={0.2}
 
         />
         <button onClick={() => setZoom(zoom + 1)}>Refresh</button>
@@ -218,16 +109,16 @@ function Input(props) {
     <button onClick={() => props.setShowData(true)}>Generate Graph</button></>)
 }
 function Table() {
-  const [edges, services, SCF, ADSA, gini] = makeCalculations();
+  const metrics = makeCalculations();
   return (<div><h1>Results</h1> <div className="table" >
 
     <table className='styled-table' id="outputTable">
       <thead><td><h3>Metrics</h3></td><td><h3>Value</h3></td></thead>
-      <tr><td>Total Microservices</td><td>{services}</td></tr>
-      <tr><td>Number of Connections</td><td>{edges}</td></tr>
-      <tr><td>SCF</td><td>{SCF}</td></tr>
-      <tr><td>ADSA</td><td>{ADSA}</td></tr>
-      <tr><td>Gini</td><td>{gini}</td></tr>
+      <tr><td>Total Microservices</td><td>{metrics.services}</td></tr>
+      <tr><td>Number of Connections</td><td>{metrics.edges}</td></tr>
+      <tr><td>SCF</td><td>{metrics.SCF}</td></tr>
+      <tr><td>ADSA</td><td>{metrics.ADSA}</td></tr>
+      <tr><td>Gini</td><td>{metrics.gini}</td></tr>
     </table>
   </div> </div>)
 }
@@ -253,27 +144,129 @@ function SystemInformation() {
 
 function CouplingEvolution() {
 
+  const evolutionJSON = systemEvolution;
+  var couplingEvolution = [];
+
+  for (let release of evolutionJSON.releases) {
+    //makeCalculation is going to return an object
+    couplingEvolution.push(makeCalculations(release));
+  }
+
+
+
+  return (<div><div className='styled-table'><table className='information-table'>
+
+    <thead>
+      <td><h3>Sr#</h3></td>
+
+      <td><h3>Release Version</h3></td>
+      <td><h3>Release Date</h3></td>
+      <td><h3>Number of Microservices</h3></td>
+      <td><h3>Number of Connections</h3></td>
+      <td><h3>SCF</h3></td>
+      <td><h3>ADSA</h3></td>
+      <td><h3>Gini(ADS)</h3></td>
+
+
+    </thead>
+    {couplingEvolution.map((item, index) =>
+      <tr>
+        <td>{index + 1}</td>
+        <td >{item.version}</td>
+        <td >{item.timestamp}</td>
+        <td>{item.services}</td>
+        <td>{item.edges}</td>
+        <td>{item.SCF}</td>
+        <td>{item.ADSA}</td>
+        <td>{item.gini}</td>
+      </tr>
+    )}</table></div>
+    <h1>Evolution Graph</h1>
+
+    <EvolutionGraph data={couplingEvolution} />
+  </div>)
+}
+
+//Reference: https://www.geeksforgeeks.org/create-a-line-chart-using-recharts-in-reactjs/
+function EvolutionGraph(props) {
+  debugger;
+  return (<div>
+    <LineChartComponent data={props.data} dataKey={"ADSA"} color={"#10C169"} />
+    <LineChartComponent data={props.data} dataKey={"gini"} color={"#F6C800"} />
+    <LineChartComponent data={props.data} dataKey={"SCF"} color={"#00BBE4"} />
+
+
+
+
+
+  </div>)
+}
+
+function LineChartComponent(props) {
+  return (<><ResponsiveContainer className='line-chart' width="100%" aspect={3}>
+    <LineChart data={props.data} margin={{ right: 300 }}>
+
+      <XAxis dataKey="version"
+        interval={'preserveStartEnd'} />
+      <YAxis></YAxis>
+      <Legend />
+      <Tooltip />
+      <Line dataKey={props.dataKey}
+        stroke={props.color} activeDot={{ r: 8 }} />
+
+
+
+    </LineChart>
+  </ResponsiveContainer></>)
 }
 
 
-// function calculateADS(graph) {
-//   let count = 0;
-//   debugger;
-//   for (let node of graph.elements.nodes) {
-//     count += graph.elements.edges.filter(x => x.data.source == node.data.id).length;
-//   }
-//   alert(count);
-//   return count;
 
-// }
-// function calculateAIS(graph) {
-//   let count = 0;
-//   for (let node of graph.elements.nodes) {
-//     count += graph.elements.edges.filter(x => x.data.target == node.data.id).length;
-//   }
-//   alert(count);
-//   return count;
-// }
+
+function transformedData() {
+
+
+  let nodes = [];
+  let links = [];
+  let nodeObject;
+  let edgeObject;
+  const obj = makeCalculations();
+
+
+
+
+  for (let edge of dependencyGraph.elements.edges) {
+    edgeObject = {};
+    edgeObject.source = edge.data.source;
+    edgeObject.target = edge.data.target;
+
+    links.push(edgeObject);
+  }
+  for (let node of dependencyGraph.elements.nodes) {
+    nodeObject = {};
+    //the API gateway service has no name. So we need to avoid that
+    if (node.data.service)
+      nodeObject.name = node.data.service;
+    else
+      nodeObject.name = "Root"
+
+    nodeObject.value = 1;
+
+    nodeObject.id = node.data.id;
+    nodeObject.ads = links.filter(x => x.source === node.data.id).length;
+    if (nodeObject.ads >= parseInt(obj.ADSA)) {
+      nodeObject.color = "red"
+    }
+    else {
+      nodeObject.color = "yellow"
+    }
+    //not inlcuding any links for the root microservice
+    nodeObject.ais = links.filter(x => x.target === node.data.id && node.data.service).length;
+    nodes.push(nodeObject);
+  }
+  return { "nodes": nodes, "links": links };
+
+}
 
 function calculateGini(graph) {
   let count = [];
@@ -290,41 +283,29 @@ function calculateGini(graph) {
 
   return result;
 }
-function makeCalculations() {
+function makeCalculations(json = null) {
 
 
-  var json = dependencyGraph;
-  const edges = json.elements.edges.filter(x => x["data"]["source"] !== undefined && x["data"]["source"] !== rootMicroserviceId).length;
+
+  if (!json)
+    json = dependencyGraph;
+
+  const edges = json.elements.edges.filter(x => x["data"]["source"] !== undefined && x["data"]["source"] !== getRootId(json.elements)).length;
   const services = json.elements.nodes.filter(x => x["data"]["service"] !== undefined).length;
   const SCF = (edges / ((services * services) - services)).toFixed(2);
   const ADSA = (edges / services).toFixed(2);
   const gini = calculateGini(json);
 
-  return [edges, services, SCF, ADSA, gini];
+
+
+  return { "edges": edges, "services": services, "SCF": SCF, "ADSA": ADSA, "gini": gini, "version": json.version, "timestamp": json.timestamp };
 
 }
 
 
-function generateGraph() {
-  var json = JSON.parse(document.getElementById("jsonPacket").value);
-  let input = document.getElementById("input");
 
-  input.style.display = "none";
 
-  let paths = [];
-  for (let edge of json.elements.edges) {
-    let source = getServiceNameById(edge.data.source);
 
-    if (source === undefined) {
-
-    }
-    // eslint-disable-next-line no-useless-concat
-    paths.push("\"" + getServiceNameById(edge.data.source) + "\"" + " -> " + "\"" + getServiceNameById(edge.data.target) + "\"");
-  }
-  console.log(paths);
-  setPathsValue(paths);
-  makeCalculations();
-}
 function getServiceNameById(id) {
   var json = JSON.parse(document.getElementById("jsonPacket").value);
   return json.elements.nodes.find(x => x["data"]["id"] === id)["data"]["service"];
